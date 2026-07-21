f4afdad752adf698a585ae7e572a4e46b32112ae
Ron <ronnieang93@gmail.com>
2026-07-21 15:24:41 +0800

    Update Road Bikes, Mountain Bikes Touring Bikes by region by channel.sql

--- Revenue SQL files/Road Bikes, Mountain Bikes Touring Bikes by region by channel.sql
+++ Revenue SQL files/Road Bikes, Mountain Bikes Touring Bikes by region by channel.sql
@@ -1,4 +1,4 @@
-/* Objective: QoQ analysis for Revenue by subcategory by country, by channel*/
+/* Objective: QoQ analysis for Revenue of top 3 contributors for subcategory for US, by channel in 2019 and 2020*/
 
 WITH CurrentQtrMargin AS
 	(SELECT
